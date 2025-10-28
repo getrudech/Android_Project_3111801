9f54db36f3975eaf519d4402d0e955829ba6e621
Getrude <getrude.langat@student.griffith.ie>
2025-10-28 14:30:39 +0000

    Initial commit-Project set up

--- app/src/main/java/com/example/dermadiaryapplication/MainActivity.kt
+++ app/src/main/java/com/example/dermadiaryapplication/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.dermadiaryapplication
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
@@ -11,23 +9,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Connect your buttons
-        val btnJournal = findViewById<Button>(R.id.btnJournal)
-        val btnCamera = findViewById<Button>(R.id.btnCamera)
-        val btnInsights = findViewById<Button>(R.id.btnInsights)
 
-        // Button click actions
-        btnJournal.setOnClickListener {
-            Toast.makeText(this, "Journal will open soon", Toast.LENGTH_SHORT).show()
-        }
-
-        btnCamera.setOnClickListener {
-            Toast.makeText(this, "Camera feature coming next", Toast.LENGTH_SHORT).show()
-        }
-
-        btnInsights.setOnClickListener {
-            Toast.makeText(this, "Insights coming soon", Toast.LENGTH_SHORT).show()
-        }
     }
 }
 
