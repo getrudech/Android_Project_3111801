3373dd4faba95c6dd81ce557b8fe06f19addd441
Getrude <getrude.langat@student.griffith.ie>
2025-11-28 11:41:47 +0000

    Added 3 new activities and made splash screen the launcher

---
+++ app/src/main/java/com/example/dermadiaryapplication/OnboardingActivity.kt
@@ -0,0 +1,47 @@
+package com.example.dermadiaryapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dermadiaryapplication.ui.theme.DermaDiaryApplicationTheme
+
+class OnboardingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DermaDiaryApplicationTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting4(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting4(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview4() {
+    DermaDiaryApplicationTheme {
+        Greeting4("Android")
+    }
+}
