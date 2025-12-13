47558c66cfc4eba60a39c5ddb2baf87833efac5e
Getrude <getrude.langat@student.griffith.ie>
2025-12-13 21:27:59 +0000

    Used a type converter to convert productsUsed: List<String> into a single String

--- app/src/main/java/com/example/dermadiaryapplication/data/db/entity/Converters.kt
+++ app/src/main/java/com/example/dermadiaryapplication/data/db/entity/Converters.kt
@@ -1,4 +1,20 @@
 package com.example.dermadiaryapplication.data.db.entity
 
+import androidx.room.TypeConverter
+
 class Converters {
+
+    // Turns a list of products (like Cleanser,Toner) into one big string ("Cleanser,Toner") for the database
+    @TypeConverter
+    fun fromListString(list: List<String>?): String? {
+        // If the list is empty or null, we just save a null
+        return list?.joinToString(separator = ",")
+    }
+
+    // Turns the big string ("Cleanser,Toner") back into a list when we load the data
+    @TypeConverter
+    fun toListString(data: String?): List<String> {
+        //if the data is null or empty, just return an empty list
+        return data?.split(",")?.map { it.trim() } ?: emptyList()
+    }
 }
